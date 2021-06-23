521d06c4111d0793445abc517187dff4f53c7fd4
Felipe C. Araujo <felipe.s-04@hotmail.com>
2021-06-22 21:14:14 -0300

    first commit

---
+++ android/app/src/main/java/com/payflow/payflow/MainActivity.java
@@ -0,0 +1,6 @@
+package com.payflow.payflow;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'ui/pages/pages.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'PayFlow',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: HomePage(),
+    );
+  }
+}
