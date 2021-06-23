39d84a7ed4813722fe73fc19da4e9f06055d3317
Felipe C. Araujo <felipe.s-04@hotmail.com>
2021-06-23 16:48:17 -0300

    refactoring config android

--- lib/ui/pages/login/login_page.dart
+++ lib/ui/pages/login/login_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 import 'package:payflow/themes/themes.dart';
 
 import './components/components.dart';
@@ -47,8 +48,20 @@ class _LoginPageState extends State<LoginPage> {
                   Padding(
                     padding: const EdgeInsets.only(left: 40,right: 40,top: 40),
                     child: ButtonSocialGoogle(
-                      onTap: (){
-                        //
+                      onTap: () async{
+                        GoogleSignIn _googleSignIn = GoogleSignIn(
+                          scopes: [
+                            'email',
+                            'https://www.googleapis.com/auth/contacts.readonly',
+                          ],
+                        );
+                        try {
+                          final response = await _googleSignIn.signIn();
+                          print("response");
+                          print(response);
+                        } catch (error) {
+                          print(error);
+                        }
                       },
                     ),
                   )
